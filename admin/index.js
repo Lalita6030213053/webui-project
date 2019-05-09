8ffbcb1a9b9fbf9dfdc26d427008ca3e984d3617
Lalita <ja.ea5923@gmail.com>
2019-05-10 03:06:52 +0700

    dashboard

--- admin/index.js
+++ admin/index.js
@@ -11,20 +11,62 @@ $(function () {
 });
 
 
-
-function Login(){
+function Login() {
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    if ( email == "admin@nw.com" && password == "12345**"){
+    if (email == "admin@nw.com" && password == "12345**") {
         console.log(true);
-    alert("Login successfully");
-    return true;
-}else{
-    console.log(false);
-    alert("Something Wrong!!")
-    return false;
-}
+        alert("Login successfully");
+        return true;
+    } else {
+        console.log(false);
+        alert("Something Wrong!!")
+        return false;
+    }
 
 }
+
+$(function () {
+
+    $.get("products.json", function (data) {
+        var i = 0;
+        for (i = 0; i <= data.length; i++) {
+            var total = 0;
+            total += i;
+        }
+        $("#getproducts").append(total);
+
+    });
+
+    $.get("customers.json", function (data) {
+        var i = 0;
+        for (i = 0; i <= data.length; i++) {
+            var total = 0;
+            total += i;
+        }
+        $("#getcustomers").append(total);
+
+    });
+
+    $.get("suppliers.json", function (data) {
+        var i = 0;
+        for (i = 0; i <= data.length; i++) {
+            var total = 0;
+            total += i;
+        }
+        $("#getsupplys").append(total);
+
+    });
+
+    $.get("orders.json", function (data) {
+        var i = 0;
+        for (i = 0; i <= data.length; i++) {
+            var total = 0;
+            total += i;
+        }
+        $("#getorders").append(total);
+
+    });
+});
